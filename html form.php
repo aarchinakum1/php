2a21d2934b9f0ebd6fcf601276c252919aa4b632
aarchinakum1 <aarchiben.nakum126978@marwadiuniversity.ac.in>
2026-06-12 18:39:00 +0530

    Add files via upload

---
+++ html form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Session Form</title>
+</head>
+<body>
+.box    
+
+<h2>Enter Username</h2>
+
+<form action="display.php" method="post">
+    Username:
+    <input type="text" name="username" required>
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
